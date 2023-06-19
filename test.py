8f5b9bcc094ab559ee3e82b7211860ce1e83a655
viktoriaukr <viktoriiauhliai@gmail.com>
2023-06-18 23:24:02 -0500

    new commit

--- test.py
+++ test.py
@@ -8,39 +8,25 @@ app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 app.config['TESTING'] = True
 
 
+
 class FlaskTests(TestCase):
 
     def test_board(self):
         with app.test_client() as client:
             res = client.get('/')
             html = res.get_data(as_text=True)
+            self.assertIn('board', session)
             self.assertEqual(res.status_code, 200)
             self.assertIn('<div>TImer: <span class="timer"></span></div>', html)
 
 
-
-    def test_check_valid_word(self):
-        boggle_game = Boggle()
-        board = [
-            ['A', 'B', 'C', 'D'],
-            ['E', 'F', 'G', 'H'],
-            ['I', 'J', 'K', 'L'],
-            ['M', 'N', 'O', 'P']
-        ]
-        word = 'DOG'
-        result = boggle_game.check_valid_word(board, word)
-        self.assertEqual(result, 'not-on-board')
+    def test_submit_guess_invalid_word(self):
+        with app.test_client() as client:
+            client.get('/')
+            response = client.get('/submit-guess?word=invalid&board=example_board')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json['result'], 'not-on-board')
 
 
 
-    def test_check_valid_word_2(self):
-        boggle_game = Boggle()
-        board = [
-            ['A', 'B', 'C', 'D'],
-            ['E', 'F', 'G', 'H'],
-            ['I', 'J', 'K', 'L'],
-            ['M', 'N', 'O', 'P']
-        ]
-        word = 'XYZ'
-        result = boggle_game.check_valid_word(board, word)
-        self.assertEqual(result, 'not-on-board')
+
